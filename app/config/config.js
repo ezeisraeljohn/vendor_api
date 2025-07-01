21643687b7e9dc3757b0aef97e29974a84bd5658
Israel Eze <ezeisraeljohn@gmail.com>
2025-07-01 23:28:43 +0100

    Add configuration file for database connection settings

---
+++ app/config/config.js
@@ -0,0 +1,64 @@
+const dotenv = require("dotenv");
+const path = require("path");
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+}
+
+const {
+  DB_DEV_HOST,
+  DB_DEV_USERNAME,
+  DB_DEV_PASSWORD,
+  DB_DEV_TYPE,
+  DB_DEV_PORT,
+  DB_DEV_DATABASE,
+} = process.env;
+module.exports = {
+  development: {
+    username: DB_DEV_USERNAME,
+    password: DB_DEV_PASSWORD,
+    database: DB_DEV_DATABASE,
+    dialect: "postgres",
+    port: DB_DEV_PORT,
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeedData.json",
+    seederStorageTableName: "sequelize_data",
+    logging: false,
+  },
+  test: {
+    username: DB_DEV_USERNAME,
+    password: DB_DEV_PASSWORD,
+    database: DB_DEV_DATABASE,
+    host: DB_DEV_HOST,
+    dialect: DB_DEV_TYPE,
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeedData.json",
+    seederStorageTableName: "sequelize_data",
+    logging: false,
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeedData.json",
+    seederStorageTableName: "sequelize_data",
+    logging: false,
+  },
+  staging: {
+    username: DB_DEV_USERNAME,
+    password: DB_DEV_PASSWORD,
+    database: DB_DEV_DATABASE,
+    host: DB_DEV_HOST,
+    dialect: "postgres",
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeedData.json",
+    seederStorageTableName: "sequelize_data",
+    logging: false,
+  },
+};
